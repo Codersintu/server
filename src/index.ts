687a820684772535bfa619a7f7de923021b53a6a
Codersintu <skmahto9199@gmail.com>
2025-10-22 09:37:59 +0530

    added frontend url

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['https://second-brain-delta-kohl.vercel.app'],
     methods:'GET,PUT,POST,DELETE',
     credentials:true
 }))
